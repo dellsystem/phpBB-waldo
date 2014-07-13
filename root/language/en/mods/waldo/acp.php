8dce9d33014688508ea1ad1dd670f14dd9c4bb0f
dellsystem <ilostwaldo@gmail.com>
2014-07-13 14:04:04 -0400

    Hide wait time setting in ACP if UPS is disabled
    Or not installed.
    
    Closes #10.

--- root/language/en/mods/waldo/acp.php
+++ root/language/en/mods/waldo/acp.php
@@ -45,7 +45,7 @@ $lang = array_merge($lang, array(
 	'WALDO_EXCLUDE'				=> "Pages to exclude",
 	'WALDO_EXCLUDE_EXPLAIN'		=> "Enter the filenames of the pages that you don't want Waldo to ever appear on, separated by a space. e.g. 'index.php viewonline.php memberlist.php'",
 	'WALDO_WAIT_TIME'			=> 'Time a user must wait before seeing Waldo again',
-	'WALDO_WAIT_TIME_EXPLAIN'	=> 'Enter the number of seconds a user must wait before he or she can see Waldo again. Set to 86400 by default to prevent users from seeing Waldo more than once per day. To disable the wait time, set this to 0.',
+	'WALDO_WAIT_TIME_EXPLAIN'	=> 'Enter the number of seconds a user must wait before he or she can see Waldo again. Set to 86400 by default to prevent users from seeing Waldo more than once per day. To disable the wait time, set this to 0. Note that this only takes effect if the Points MOD is enabled and the number of points awarded per Waldo view is non-zero.',
 ));
 
 ?>
